fb0a3c73bae70cb158e68a7ecbcf09413b3acf87
falaesticados <145461191+laurinha14@users.noreply.github.com>
2023-10-03 11:13:12 -0300

    prova de sergio
    laura moreira
    jussara freitas

---
+++ prova.py
@@ -0,0 +1,9 @@
+n1= int(input("digite sua primeira nota"))
+n2= int(input("digite sua segunda nota"))
+n3= int(input("digite sua terceira nota"))
+n4= int(input("digite sua quarta nota"))
+r= (n1+n2+n3+n4)/4
+print("sua media e:", r)
+
+
+       
